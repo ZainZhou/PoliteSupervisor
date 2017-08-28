4355d9bc1cee1ee46aee985d87072b707e05cc58
T&H <truemenhale@gmail.com>
2017-08-28 20:32:58 +0800

    startPage

--- Public/js/PageChange.js
+++ Public/js/PageChange.js
@@ -8,13 +8,24 @@ $(function(){
     var Bc = $('.BtnContainer');
     var Gc = $('.GamelogoContainer');
     var Ic = $('.InstructionContainer');
+    var Cr = $('.copyright');
+    var DBtn = $('.DeveloperBtn');
+    var oMask = $('.mask');
+    var DeveloperList = $('.DeveloperList');
+    var closeBtn = $('.closeBtn');
+    closeBtn.on('click',function(){
+        oMask.css('display','none');
+        DeveloperList.css('display','none');
+    });
     insBtn.on('click',function(){
         insBtn.css({'display':'none'});
         startBtn.css({'display':'none'});
         getBtn.css({'display':'block'});
         Gc.css({'display':'none'});
         Ic.css({'display':'block'});
-        Bc.css({'margin':'8% auto'});
+        Bc.css({'margin':'5.2% auto'});
+        Cr.css({'display':'none'});
+        DBtn.css({'display':'block'});
     });
     getBtn.on('click',function(){
         insBtn.css({'display':'block'});
@@ -23,5 +34,11 @@ $(function(){
         Gc.css({'display':'block'});
         Ic.css({'display':'none'});
         Bc.css({'margin':'12% auto'});
+        Cr.css({'display':'block'});
+        DBtn.css({'display':'none'});
+    });
+    DBtn.on('click',function(){
+        oMask.css('display','block');
+        DeveloperList.css('display','block');
     });
 });
---
+++ Public/js/SetStyle.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Zain on 28/8/2017.
+ */
+$(function(){
+    $('.DeveloperList').css({'height':$(window).width()*0.84});
+});
