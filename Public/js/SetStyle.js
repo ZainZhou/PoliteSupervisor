1ae49e4de8706026ef139f1524a87265c652ad8f
T&H <truemenhale@gmail.com>
2017-08-29 17:06:48 +0800

    GamePageCSS

--- Public/js/SetStyle.js
+++ Public/js/SetStyle.js
@@ -5,4 +5,10 @@ $(function(){
     $('.DeveloperList').css({'height':$(window).width()*0.84});
     $('.recordContainer').css({'height':$(window).width()*0.1573});
     $('.wordContainer').css({'height':$(window).width()*1.396});
+    var aLi = $('.wordUl').find('li');
+    aLi.css('line-height',$(window).width()*0.13389+'px');
+    for(var i = 0 ; i < aLi.length ; i++){
+        if(i%3 == 2)continue;
+        aLi.eq(i).css('margin-right','5.8%');
+    }
 });
